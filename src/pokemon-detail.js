88b55ba7141ff6a3757812d7e9ebee1b81796dce
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-05-12 20:30:26 +0200

    Track acync request's in useState hook

--- src/pokemon-detail.js
+++ src/pokemon-detail.js
@@ -1,9 +1,20 @@
+import React, { useState } from 'react'
 import { fetchPokemon, suspensify } from './api'
 
-import React from 'react'
-
-let pokemon = suspensify(fetchPokemon(1))
+let initialPokemon = suspensify(fetchPokemon(1))
 
 export default function PokemonDetail() {
-	return <div>{pokemon.read().name}</div>
+	const [pokemonResource, setPokemonResource] = useState(initialPokemon)
+	const pokemon = pokemonResource.read()
+
+	return (
+		<div>
+			{pokemon.name}
+			<button
+				type='button'
+				onClick={() => setPokemonResource(suspensify(fetchPokemon(pokemon.id + 1)))}>
+				Button
+			</button>
+		</div>
+	)
 }
