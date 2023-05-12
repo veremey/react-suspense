e76987f74e8ffbd6a4aba571d872f513469435b5
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-05-12 17:11:37 +0200

    React.Lazy and promise statuses

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import ErrorBoundary from './error-boundary'
 import React from 'react'
 // const PokemonDetail = React.lazy(() => import('./pokemon-detail'))
-const PokemonDetail = React.lazy(() => Promise.reject())
-// app.js
+const PokemonDetail = React.lazy(
+	() =>
+		new Promise((resolve) =>
+			setTimeout(resolve({ default: () => <div>[ Fake ] Pokemon</div> }), 1000)
+		)
+)
+
+// promises statuses works diferent
+// resolve  || data module inside
+// reject
+// pending
 
 export default function App() {
 	return (
