63eaf147ce9560d2281730aefed7b0de927affd2
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-05-12 20:06:11 +0200

    wrap fetch requests to communicate with Promise

--- src/pokemon-detail.js
+++ src/pokemon-detail.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React from 'react'
+const API_LINK = 'https://pokeapi.co/api/v2/pokemon/1'
+
+function suspensify(promise) {
+	let status = 'pending'
+	let result
+	let suspender = promise.then(
+		(response) => {
+			status = 'success'
+			result = response
+		},
+		(error) => {
+			status = 'error'
+			result = error
+		}
+	)
+
+	return {
+		read() {
+			if (status === 'pending') {
+				throw suspender
+			}
+			if (status === 'error') {
+				throw result
+			}
+			if (status === 'success') {
+				return result
+			}
+		},
+	}
+}
+
+let pokemon = suspensify(fetch(API_LINK).then((res) => res.json()))
 
 export default function PokemonDetail() {
-  return <div>Static Pokemon</div>;
+	return <div>{pokemon.read().name}</div>
 }
